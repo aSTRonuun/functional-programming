42507a5bdea98d9c3a3284c6def930e0006fb6b7
Vitor Alves <vitormanoel00710@gmail.com>
2022-04-23 17:03:02 -0300

    Update algorithm, resulition bug

--- 04S-basic-algorithms/12_sublist.hs
+++ 04S-basic-algorithms/12_sublist.hs
@@ -1,3 +1,11 @@
 sublist init' end' xs =  drop init . take end $ xs
-    where init = if init' < 0 then  init' + (length xs) else init'
-          end = if end' < 0 then end' + (length xs) else end'
+    where init = if init' < 0 then  init' `mod` length xs else init'
+          end = if end' < 0 then end' `mod` length xs else end'
+
+main = do
+    print $ sublist 1 3 [0,1,2,3,4,5,6,7,8,9,10]
+    print $ sublist 0 11 [0,1,2,3,4,5,6,7,8,9,10]
+    print $ sublist 2 8 [0,1,2,3,4,5,6,7,8,9,10]
+    print $ sublist 2 (-2) [0,1,2,3,4,5,6,7,8,9,10]
+    print $ sublist (-4) (-2) [0,1,2,3,4,5]
+    print $ sublist (-10) (-1) [0,1,2,3,4,5,6,7,8,9,10]
---
+++ 05S-exercicies-fold/contar.hs
@@ -0,0 +1,27 @@
+--count using fold
+contar x xs = foldl count x xs
+    where count x y = 
+
+
+
+--contar_todos xs 
+
+
+
+--menor xs
+
+
+
+--existe x xs
+
+
+
+--impares xs
+
+
+
+--quantos_vizinhos_igual xs
+
+
+
+--eh_primo x
