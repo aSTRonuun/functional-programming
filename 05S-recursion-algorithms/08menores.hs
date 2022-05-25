6c5736aaa1bb4f1238355db1c09e3343ddffccae
Vitor Alves <vitormanoel00710@gmail.com>
2022-05-25 10:58:48 -0300

    Added execices functionais

--- 05S-recursion-algorithms/08menores.hs
+++ 05S-recursion-algorithms/08menores.hs
@@ -3,3 +3,4 @@ menores y (x:xs) = if length (x:xs) > y then menores y xs else x:menores y xs
 
 main = do
     print $ menores 5 [6,2,3,4,9]
+    print $ 
--- 05S-recursion-algorithms/removerMaior.hs
+++ 05S-recursion-algorithms/removerMaior.hs
@@ -1,7 +1,12 @@
+maiorElemento xs = foldr max 0 xs
+
 removerMaior [] = []
 removerMaior [x] = []
-removerMaior (x:xs) = 
+removerMaior (x:xs) = if x == maiorElemento (x:xs) then removerMaior xs else x:removerMaior xs
 
 
 main = do
-    print $ removerMaior [6,2,3,4,9]
+    print $ removerMaior [1]
+    print $ removerMaior [1,3] 
+    print $ removerMaior [1,3,5] 
+    print $ removerMaior [1,3,5,2] 
